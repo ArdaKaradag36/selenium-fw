d326b557ffb44e14ea04df1785d05849f14448b8
ArdaKaradag36 <karadagardaa@gmail.com>
2026-02-13 16:48:02 +0300

    reggresion yapıldı

--- utils/browser_factory.py
+++ utils/browser_factory.py
@@ -31,6 +31,9 @@ def get_driver():
         if HEADLESS:
             options.add_argument("--headless=new")
             options.add_argument("--disable-gpu")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--window-size=1920,1080")
 
         # PATH içindeki chromedriver otomatik kullanılacak
         driver = webdriver.Chrome(
